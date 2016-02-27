1a4d6049a79cb8586ec174af23f5cb5b37527224
Vicente de Miguel <spidvmp@gmail.com>
2016-02-27 13:27:53 +0100

    Funciones de grabacion del login localmente

--- Scoops/Utils/Defines.swift
+++ Scoops/Utils/Defines.swift
@@ -21,3 +21,30 @@ func getMSClient() -> MSClient {
     )
 }
 
+
+//metodos para guardar el login de facebook localmente y poderlo recuperar
+func saveAuthInfo (currentUser : MSUser?){
+    NSUserDefaults.standardUserDefaults().setObject(currentUser?.userId, forKey: "userId")
+    NSUserDefaults.standardUserDefaults().setObject(currentUser?.mobileServiceAuthenticationToken, forKey: "tokenId")
+}
+
+
+func loadUserAuthInfo() -> (usr : String, tok : String)? {
+    let user = NSUserDefaults.standardUserDefaults().objectForKey("userId") as? String
+    let token = NSUserDefaults.standardUserDefaults().objectForKey("tokenId") as? String
+    
+    return (user!, token!)
+}
+
+func isUserloged() -> Bool {
+    
+    var result = false
+    
+    let userID = NSUserDefaults.standardUserDefaults().objectForKey("userId") as? String
+    if let _ = userID {
+        result = true
+    }
+    return result
+    
+}
+
